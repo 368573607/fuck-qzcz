7fab7f7ed51f727cab7e8ac75c0b52ab787d7243
sun <xuhuading@foxmail.com>
2022-02-12 23:41:24 +0800

    初步

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>OHHH!!</h1>
+  <h1>还有 {{ Math.trunc((targetTime - currentTime) / 1000) }} 秒开学</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      targetTime: new Date(2022, 1, 16, 8, 0, 0),
+      currentTime: new Date(),
+    };
+  },
+  mounted: function () {
+    const self = this;
+    this.timer = setInterval(function () {
+      self.currentTime = new Date();
+    }, 1000);
+  },
+  unmounted: function () {
+    clearInterval(this.timer);
+  },
+  computed: {
+    remainingTime() {
+      return Math.trunc((this.targetTime - this.currentTime) / 1000);
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
